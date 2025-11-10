74f3fe52290fcdbdf7d75a133dc4c90239e80e88
rudya55 <fasttransport26@gmail.com>
2025-11-10 11:21:55 +0000

    ci: add android workflow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.4abdee7f238d436b9d0d34c8665e5ddf',
+  appId: 'com.lovable.drivervtcdispatch',
   appName: 'drivervtcdispatch',
   webDir: 'dist',
   server: {
